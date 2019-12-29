2c829cbb89f74b4039a041bd6f8c0ae5bc34446c
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2019-12-29 08:42:15 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-"the many manners you Have are fine with me".match(/\[A-Z]\)
+"the many manners you Have are fine with me".match(/\[A-Z]\$+)
 end
 
 def valid_phone_number?(phone)
