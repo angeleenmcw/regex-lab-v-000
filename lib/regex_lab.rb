8de660482efc35d96560ef21062bdcfdffae92d4
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2019-12-29 08:34:44 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-"I do not like to underlying truth".scan(/)
+"I do not like to underlying truth".scan(/|^+un,||)
 end
 
 def words_five_letters_long(text)
