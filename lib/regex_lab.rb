511d46ec53696e951e13f078aaffd6643ff669e9
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2019-12-29 08:39:21 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,13 @@
 def starts_with_a_vowel?(word)
-"time to eat the ice cream".scan(/|D[aeiou]/)
+"time to eat the ice cream".scan(/\D[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-"I do not like to underlying truth".scan(/|^+"un",|$+"ing")
+"I do not like to underlying truth".scan(/\^+"un",\$+"ing")
 end
 
 def words_five_letters_long(text)
-"plain main fain mouse house tally".scan(/)
+"plain main fain mouse house tally".scan(/\a)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
