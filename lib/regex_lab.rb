b0e8907718d6f3012149aba14422d8f9b1e4011f
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2019-12-29 08:46:27 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-"I do not like to underlying truth".scan(/\^+"un",\$+"ing")
+"I do not like to underlying truth".scan(/[\^+"un"\$+"ing"])
 end
 
 def words_five_letters_long(text)
@@ -11,7 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-"the many manners you Have are fine with me".match(/\[A-Z]\$+\W""""/)
+"the many manners you Have are fine with me".match(/[\[A-Z]\$\W""""]/)
 end
 
 def valid_phone_number?(phone)
